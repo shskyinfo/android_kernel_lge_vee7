16efcc155fd735f962c0fb23a5831f30e1f62b2c
Suhail <sh.skyinfo@gmail.com>
2015-05-01 19:44:05 +0530

    Testing msm7x27 series overclock

--- arch/arm/mach-msm/acpuclock-7627.c
+++ arch/arm/mach-msm/acpuclock-7627.c
@@ -191,7 +191,7 @@ static struct clkctl_acpu_speed pll0_960_pll1_196_pll2_800_pll4_0[] = {
 /* 7627a PLL2 @ 1200MHz with GSM capable modem */
 static struct clkctl_acpu_speed pll0_960_pll1_245_pll2_1200_pll4_800[] = {
 	{ 0, 19200, ACPU_PLL_TCXO, 0, 0, 2400, 3, 0, 30720 },
-	{ 0, 61440, ACPU_PLL_1, 1, 3,  7680, 3, 1,  61440 },
+	{ 1, 61440, ACPU_PLL_1, 1, 3,  7680, 3, 1,  61440 },
 	{ 1, 122880, ACPU_PLL_1, 1, 1,  15360, 3, 2,  61440 },
 	{ 1, 245760, ACPU_PLL_1, 1, 0, 30720, 3, 3,  61440 },
 	{ 0, 300000, ACPU_PLL_2, 2, 3, 37500, 3, 4, 122880 },
@@ -199,7 +199,15 @@ static struct clkctl_acpu_speed pll0_960_pll1_245_pll2_1200_pll4_800[] = {
 	{ 0, 400000, ACPU_PLL_4, 6, 1, 50000, 3, 4, 122880 },
 	{ 1, 480000, ACPU_PLL_0, 4, 1, 60000, 3, 5, 122880 },
 	{ 1, 600000, ACPU_PLL_2, 2, 1, 75000, 3, 6, 160000 },
-	{ 1, 800000, ACPU_PLL_4, 6, 0, 100000, 3, 7, 200000 },
+/*Now undervolt is enabled by default*/
+	{ 1, 800000, ACPU_PLL_4, 6, 0, 100000, 3, 6, 200000 },
+#ifdef CONFIG_MSM7X27A_OVERCLOCK
+        { 1, 900000, ACPU_PLL_2, 2, 0, 112500, 3, 7, 200000 },
+        { 1, 950000, ACPU_PLL_2, 2, 0, 118750, 3, 7, 200000 },
+        { 1, 1000000, ACPU_PLL_2, 2, 0, 125000, 3, 7, 200000 },        
+        { 1, 1050000, ACPU_PLL_2, 2, 0, 131250, 3, 7, 200000 },
+        { 1, 1100000, ACPU_PLL_2, 2, 0, 137500, 3, 7, 200000 },	
+#endif
 	{ 0 }
 };
 
@@ -214,7 +222,15 @@ static struct clkctl_acpu_speed pll0_960_pll1_196_pll2_1200_pll4_800[] = {
 	{ 0, 400000, ACPU_PLL_4, 6, 1, 50000, 3, 4, 120000 },
 	{ 1, 480000, ACPU_PLL_0, 4, 1, 60000, 3, 5, 120000 },
 	{ 1, 600000, ACPU_PLL_2, 2, 1, 75000, 3, 6, 160000 },
-	{ 1, 800000, ACPU_PLL_4, 6, 0, 100000, 3, 7, 200000 },
+/*Now undervolt is enabled by default*/
+	{ 1, 800000, ACPU_PLL_4, 6, 0, 100000, 3, 6, 200000 },
+#ifdef CONFIG_MSM7X27A_OVERCLOCK
+        { 1, 900000, ACPU_PLL_2, 2, 0, 112500, 3, 7, 200000 },
+        { 1, 950000, ACPU_PLL_2, 2, 0, 118750, 3, 7, 200000 },
+        { 1, 1000000, ACPU_PLL_2, 2, 0, 125000, 3, 7, 200000 },        
+        { 1, 1050000, ACPU_PLL_2, 2, 0, 131250, 3, 7, 200000 },
+        { 1, 1100000, ACPU_PLL_2, 2, 0, 137500, 3, 7, 200000 },	
+#endif
 	{ 0 }
 };
 
@@ -615,10 +631,22 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s)
 {
 	uint32_t reg_clkctl, reg_clksel, clk_div, src_sel;
 
+ 	#ifdef CONFIG_MSM7X27A_OVERCLOCK	
+	uint32_t a11_div;	
+ 	#endif
+
 	reg_clksel = readl_relaxed(A11S_CLK_SEL_ADDR);
 
 	/* AHB_CLK_DIV */
 	clk_div = (reg_clksel >> 1) & 0x03;
+#ifdef CONFIG_MSM7X27A_OVERCLOCK
+	a11_div=hunt_s->a11clk_src_div;
+         if(hunt_s->a11clk_khz>800000) {
+         a11_div=0;
+         writel(hunt_s->a11clk_khz/19200, MSM_CLK_CTL_BASE+0x33C);
+         udelay(50);
+}
+#endif
 	/* CLK_SEL_SRC1NO */
 	src_sel = reg_clksel & 1;
 
